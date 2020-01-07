df398b08d80a8c374be85e66a67d6288ced51165
Sebastian Maleck <sebastian.maleck@kolibrigames.com>
2020-01-07 08:32:30 +0100

    Adds sample Audio

---
+++ Assets/UGF/Services/Audio/AudioSourceItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UGF.Util.Mono;
+using UnityEngine;
+
+namespace UGF.Services.Audio
+{
+    public class AudioSourceItem : AbstractDisposableMonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+    }
+}
