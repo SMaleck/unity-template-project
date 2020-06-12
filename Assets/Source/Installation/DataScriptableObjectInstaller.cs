7d18d7354acc2bb60d80723b8e20ac15ae4f7678
Sebastian Maleck <sebastian.maleck@kolibrigames.com>
2020-06-12 14:06:31 +0200

    Consolidates installation

--- Assets/Source/Installation/DataScriptableObjectInstaller.cs
+++ Assets/Source/Installation/DataScriptableObjectInstaller.cs
@@ -1,23 +1,30 @@
+﻿using Source.Framework;
 using Source.Framework.Services.Audio.Config;
 using Source.Framework.Services.Savegames.Config;
 using Source.Framework.Services.SceneManagement.Config;
+using Source.Installation.Config;
+using Source.Services.AudioPlayer.Config;
 using UnityEngine;
 using Zenject;
 
-namespace Source.Framework.Installation
+namespace Source.Installation
 {
-    [CreateAssetMenu(fileName = nameof(UgfDataInstaller), menuName = Constants.InstallersMenu + nameof(UgfDataInstaller))]
-    public class UgfDataInstaller : ScriptableObjectInstaller<UgfDataInstaller>
+    [CreateAssetMenu(fileName = nameof(DataScriptableObjectInstaller), menuName = Constants.InstallersMenu + nameof(DataScriptableObjectInstaller))]
+    public class DataScriptableObjectInstaller : ScriptableObjectInstaller<DataScriptableObjectInstaller>
     {
         [SerializeField] private SceneManagementConfig _sceneManagementConfig;
         [SerializeField] private SavegamesConfig _savegamesConfig;
         [SerializeField] private AudioServiceConfig _audioServiceConfig;
+        [SerializeField] private ViewPrefabConfig _viewPrefabConfig;
+        [SerializeField] private AudioClipsConfig _audioClipsConfig;
 
         public override void InstallBindings()
         {
             Container.BindInstance(_sceneManagementConfig);
             Container.BindInstance(_savegamesConfig);
             Container.BindInstance(_audioServiceConfig);
+            Container.BindInstance(_viewPrefabConfig);
+            Container.BindInstance(_audioClipsConfig);
         }
     }
 }
--- Assets/Source/Installation/ProjectInstaller.cs
+++ Assets/Source/Installation/ProjectInstaller.cs
@@ -1,13 +1,44 @@
-﻿using Source.Services.AudioPlayer;
+﻿using Source.Framework.Initialization;
+using Source.Framework.Services.Audio;
+using Source.Framework.Services.Audio.Config;
+using Source.Framework.Services.Savegames;
+using Source.Framework.Services.SceneManagement;
+using Source.Framework.Services.SceneManagement.LoadingScreen;
+using Source.Framework.Util;
+using Source.Framework.Util.DataStorageStrategies;
+using Source.Services.AudioPlayer;
 using Source.Services.SceneTransition;
+using UnityEngine;
 using Zenject;
 
 namespace Source.Installation
 {
     public class ProjectInstaller : MonoInstaller
     {
+        [Inject] private AudioServiceConfig _audioServiceConfig;
+
         public override void InstallBindings()
         {
+            Application.targetFrameRate = 60;
+
+            Container.BindExecutionOrder<ISceneInitializer>(998);
+            Container.BindInterfacesAndSelfTo<UgfProjectInitializer>().AsSingle().NonLazy();
+
+            Container.BindInterfacesAndSelfTo<JsonDataStorageStrategy>().AsSingle();
+            Container.BindInterfacesAndSelfTo<SavegameService>().AsSingle();
+            Container.BindInterfacesAndSelfTo<SavegamePersistenceScheduler>().AsSingle();
+
+            Container.BindInterfacesAndSelfTo<SceneManagementService>().AsSingle();
+            Container.BindInterfacesAndSelfTo<SceneManagementModel>().AsSingle();
+            Container.BindInterfacesAndSelfTo<LoadingScreenModel>().AsSingle();
+            Container.BindPrefabFactory<LoadingScreenView, LoadingScreenView.Factory>();
+
+            Container.BindMemoryPool<AudioSourceItem, AudioSourceItem.Pool>()
+                .FromComponentInNewPrefab(_audioServiceConfig.AudioSourcePrefab)
+                .UnderTransformGroup("Audio");
+
+            Container.BindInterfacesAndSelfTo<AudioService>().AsSingle().NonLazy();
+
             Container.BindInterfacesAndSelfTo<AudioPlayerService>().AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<SceneTransitionService>().AsSingle().NonLazy();
         }
