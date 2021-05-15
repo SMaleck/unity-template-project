37ae5879f1ceb28a9ae53fb63e945ebf7c3e5540
SMaleck <s.maleck@live.de>
2021-05-15 14:54:08 +0200

    Adds logging

---
+++ Assets/Source/Frameworks/Logging/DefaultSavegameLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Source.Frameworks.Logging
+{
+    public class DefaultSavegameLogger : ISavegameLogger
+    {
+        private const string Prefix = "[Savegame]";
+
+        public void Log(string message)
+        {
+            UnityEngine.Debug.Log($"{Prefix} {message}");
+        }
+
+        public void Error(string message)
+        {
+            UnityEngine.Debug.LogError($"{Prefix} {message}");
+        }
+
+        public void Error(Exception e)
+        {
+            UnityEngine.Debug.LogError(e);
+        }
+    }
+}
---
+++ Assets/Source/Frameworks/Logging/ISavegameLogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Source.Frameworks.Logging
+{
+    public interface ISavegameLogger
+    {
+        void Log(string message);
+        void Error(string message);
+        void Error(Exception e);
+    }
+}
